19dfad35de65a3f725703c0d4e97edf79304c769
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-04 18:08:05 +0800

    Updated test.

--- bluesea-test.js
+++ bluesea-test.js
@@ -1,7 +1,9 @@
 "use strict";
 
 require( "olivant" );
-
+const assert = require( "assert" );
 const bluesea = require( "./bluesea.js" );
 
-bluesea( Fatal );
+assert.ok( bluesea( Fatal ) );
+
+console.log( "ok" );
